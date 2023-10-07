caa4d0f48dede9a7f2c5d9c49fef5a11004d1a17
Terrence <thosang@gmail.com>
2023-10-07 05:50:17 -0400

    Fixed PNG Palette Name labels

---
+++ app/files/TestForPng/TestForPng-color-palette.dart
@@ -0,0 +1,373 @@
+import 'package:flutter/material.dart';
+
+class MyPalette {
+  static const Color primary100 = Color(0xffe5e5);
+  static const Color primary200 = Color(0xffcccc);
+  static const Color primary300 = Color(0xffb2b2);
+  static const Color primary400 = Color(0xff9999);
+  static const Color primary500 = Color(0xf27979);
+  static const Color primary600 = Color(0xc54f4f);
+  static const Color primary700 = Color(0x992d2d);
+  static const Color primary800 = Color(0x6c1515);
+  static const Color primary900 = Color(0x3f0606);
+  static const Color secondary100 = Color(0xf9feff);
+  static const Color secondary200 = Color(0xe0fdff);
+  static const Color secondary300 = Color(0xc6fdff);
+  static const Color secondary400 = Color(0xa5f0f3);
+  static const Color secondary500 = Color(0x7ad0d3);
+  static const Color secondary600 = Color(0x50a4a7);
+  static const Color secondary700 = Color(0x2e777a);
+  static const Color secondary800 = Color(0x154b4d);
+  static const Color secondary900 = Color(0x052021);
+  static const Color tertiary100 = Color(0xe8eeff);
+  static const Color tertiary200 = Color(0xcedcff);
+  static const Color tertiary300 = Color(0xb5caff);
+  static const Color tertiary400 = Color(0x97b3f8);
+  static const Color tertiary500 = Color(0x6e8cd8);
+  static const Color tertiary600 = Color(0x4663ac);
+  static const Color tertiary700 = Color(0x27407f);
+  static const Color tertiary800 = Color(0x112352);
+  static const Color tertiary900 = Color(0x040d26);
+  static const Color neutral100 = Color(0xffffff);
+  static const Color neutral200 = Color(0xffffff);
+  static const Color neutral300 = Color(0xf2f2f2);
+  static const Color neutral400 = Color(0xd2c7c7);
+  static const Color neutral500 = Color(0xb29797);
+  static const Color neutral600 = Color(0x856464);
+  static const Color neutral700 = Color(0x593a3a);
+  static const Color neutral800 = Color(0x2c1818);
+  static const Color neutral900 = Color(0x000000);
+  static const Color green100 = Color(0xffffff);
+  static const Color green200 = Color(0xe5ffeb);
+  static const Color green300 = Color(0xccffd7);
+  static const Color green400 = Color(0xb2ffc4);
+  static const Color green500 = Color(0x8eeda4);
+  static const Color green600 = Color(0x60c076);
+  static const Color green700 = Color(0x3b934f);
+  static const Color green800 = Color(0x1e672f);
+  static const Color green900 = Color(0x0b3a16);
+  static const Color orange100 = Color(0xffffff);
+  static const Color orange200 = Color(0xffefe5);
+  static const Color orange300 = Color(0xffdfcc);
+  static const Color orange400 = Color(0xffcfb2);
+  static const Color orange500 = Color(0xedb28e);
+  static const Color orange600 = Color(0xc08560);
+  static const Color orange700 = Color(0x935d3b);
+  static const Color orange800 = Color(0x673a1e);
+  static const Color orange900 = Color(0x3a1d0b);
+  static const Color red100 = Color(0xffffff);
+  static const Color red200 = Color(0xffe5e5);
+  static const Color red300 = Color(0xffcccc);
+  static const Color red400 = Color(0xffb2b2);
+  static const Color red500 = Color(0xed8e8e);
+  static const Color red600 = Color(0xc06060);
+  static const Color red700 = Color(0x933b3b);
+  static const Color red800 = Color(0x671e1e);
+  static const Color red900 = Color(0x3a0b0b);
+  static const Color blue100 = Color(0xffffff);
+  static const Color blue200 = Color(0xe5f4ff);
+  static const Color blue300 = Color(0xcceaff);
+  static const Color blue400 = Color(0xb2e0ff);
+  static const Color blue500 = Color(0x8ec7ed);
+  static const Color blue600 = Color(0x609ac0);
+  static const Color blue700 = Color(0x3b7093);
+  static const Color blue800 = Color(0x1e4a67);
+  static const Color blue900 = Color(0x0b273a);
+
+  // Define ThemeData
+  static ThemeData myAppTheme = ThemeData(
+    primaryColor: Primary500,  // Replace with your primary color
+    accentColor: Secondary500,  // Replace with your secondary color
+    // Define other theme properties as needed
+  );
+
+  // Define custom text styles
+  static TextStyle primary100TextStyle = TextStyle(
+    color: primary100,
+    // Define text styling properties as needed
+  );
+  static TextStyle primary200TextStyle = TextStyle(
+    color: primary200,
+    // Define text styling properties as needed
+  );
+  static TextStyle primary300TextStyle = TextStyle(
+    color: primary300,
+    // Define text styling properties as needed
+  );
+  static TextStyle primary400TextStyle = TextStyle(
+    color: primary400,
+    // Define text styling properties as needed
+  );
+  static TextStyle primary500TextStyle = TextStyle(
+    color: primary500,
+    // Define text styling properties as needed
+  );
+  static TextStyle primary600TextStyle = TextStyle(
+    color: primary600,
+    // Define text styling properties as needed
+  );
+  static TextStyle primary700TextStyle = TextStyle(
+    color: primary700,
+    // Define text styling properties as needed
+  );
+  static TextStyle primary800TextStyle = TextStyle(
+    color: primary800,
+    // Define text styling properties as needed
+  );
+  static TextStyle primary900TextStyle = TextStyle(
+    color: primary900,
+    // Define text styling properties as needed
+  );
+  static TextStyle secondary100TextStyle = TextStyle(
+    color: secondary100,
+    // Define text styling properties as needed
+  );
+  static TextStyle secondary200TextStyle = TextStyle(
+    color: secondary200,
+    // Define text styling properties as needed
+  );
+  static TextStyle secondary300TextStyle = TextStyle(
+    color: secondary300,
+    // Define text styling properties as needed
+  );
+  static TextStyle secondary400TextStyle = TextStyle(
+    color: secondary400,
+    // Define text styling properties as needed
+  );
+  static TextStyle secondary500TextStyle = TextStyle(
+    color: secondary500,
+    // Define text styling properties as needed
+  );
+  static TextStyle secondary600TextStyle = TextStyle(
+    color: secondary600,
+    // Define text styling properties as needed
+  );
+  static TextStyle secondary700TextStyle = TextStyle(
+    color: secondary700,
+    // Define text styling properties as needed
+  );
+  static TextStyle secondary800TextStyle = TextStyle(
+    color: secondary800,
+    // Define text styling properties as needed
+  );
+  static TextStyle secondary900TextStyle = TextStyle(
+    color: secondary900,
+    // Define text styling properties as needed
+  );
+  static TextStyle tertiary100TextStyle = TextStyle(
+    color: tertiary100,
+    // Define text styling properties as needed
+  );
+  static TextStyle tertiary200TextStyle = TextStyle(
+    color: tertiary200,
+    // Define text styling properties as needed
+  );
+  static TextStyle tertiary300TextStyle = TextStyle(
+    color: tertiary300,
+    // Define text styling properties as needed
+  );
+  static TextStyle tertiary400TextStyle = TextStyle(
+    color: tertiary400,
+    // Define text styling properties as needed
+  );
+  static TextStyle tertiary500TextStyle = TextStyle(
+    color: tertiary500,
+    // Define text styling properties as needed
+  );
+  static TextStyle tertiary600TextStyle = TextStyle(
+    color: tertiary600,
+    // Define text styling properties as needed
+  );
+  static TextStyle tertiary700TextStyle = TextStyle(
+    color: tertiary700,
+    // Define text styling properties as needed
+  );
+  static TextStyle tertiary800TextStyle = TextStyle(
+    color: tertiary800,
+    // Define text styling properties as needed
+  );
+  static TextStyle tertiary900TextStyle = TextStyle(
+    color: tertiary900,
+    // Define text styling properties as needed
+  );
+  static TextStyle neutral100TextStyle = TextStyle(
+    color: neutral100,
+    // Define text styling properties as needed
+  );
+  static TextStyle neutral200TextStyle = TextStyle(
+    color: neutral200,
+    // Define text styling properties as needed
+  );
+  static TextStyle neutral300TextStyle = TextStyle(
+    color: neutral300,
+    // Define text styling properties as needed
+  );
+  static TextStyle neutral400TextStyle = TextStyle(
+    color: neutral400,
+    // Define text styling properties as needed
+  );
+  static TextStyle neutral500TextStyle = TextStyle(
+    color: neutral500,
+    // Define text styling properties as needed
+  );
+  static TextStyle neutral600TextStyle = TextStyle(
+    color: neutral600,
+    // Define text styling properties as needed
+  );
+  static TextStyle neutral700TextStyle = TextStyle(
+    color: neutral700,
+    // Define text styling properties as needed
+  );
+  static TextStyle neutral800TextStyle = TextStyle(
+    color: neutral800,
+    // Define text styling properties as needed
+  );
+  static TextStyle neutral900TextStyle = TextStyle(
+    color: neutral900,
+    // Define text styling properties as needed
+  );
+  static TextStyle green100TextStyle = TextStyle(
+    color: green100,
+    // Define text styling properties as needed
+  );
+  static TextStyle green200TextStyle = TextStyle(
+    color: green200,
+    // Define text styling properties as needed
+  );
+  static TextStyle green300TextStyle = TextStyle(
+    color: green300,
+    // Define text styling properties as needed
+  );
+  static TextStyle green400TextStyle = TextStyle(
+    color: green400,
+    // Define text styling properties as needed
+  );
+  static TextStyle green500TextStyle = TextStyle(
+    color: green500,
+    // Define text styling properties as needed
+  );
+  static TextStyle green600TextStyle = TextStyle(
+    color: green600,
+    // Define text styling properties as needed
+  );
+  static TextStyle green700TextStyle = TextStyle(
+    color: green700,
+    // Define text styling properties as needed
+  );
+  static TextStyle green800TextStyle = TextStyle(
+    color: green800,
+    // Define text styling properties as needed
+  );
+  static TextStyle green900TextStyle = TextStyle(
+    color: green900,
+    // Define text styling properties as needed
+  );
+  static TextStyle orange100TextStyle = TextStyle(
+    color: orange100,
+    // Define text styling properties as needed
+  );
+  static TextStyle orange200TextStyle = TextStyle(
+    color: orange200,
+    // Define text styling properties as needed
+  );
+  static TextStyle orange300TextStyle = TextStyle(
+    color: orange300,
+    // Define text styling properties as needed
+  );
+  static TextStyle orange400TextStyle = TextStyle(
+    color: orange400,
+    // Define text styling properties as needed
+  );
+  static TextStyle orange500TextStyle = TextStyle(
+    color: orange500,
+    // Define text styling properties as needed
+  );
+  static TextStyle orange600TextStyle = TextStyle(
+    color: orange600,
+    // Define text styling properties as needed
+  );
+  static TextStyle orange700TextStyle = TextStyle(
+    color: orange700,
+    // Define text styling properties as needed
+  );
+  static TextStyle orange800TextStyle = TextStyle(
+    color: orange800,
+    // Define text styling properties as needed
+  );
+  static TextStyle orange900TextStyle = TextStyle(
+    color: orange900,
+    // Define text styling properties as needed
+  );
+  static TextStyle red100TextStyle = TextStyle(
+    color: red100,
+    // Define text styling properties as needed
+  );
+  static TextStyle red200TextStyle = TextStyle(
+    color: red200,
+    // Define text styling properties as needed
+  );
+  static TextStyle red300TextStyle = TextStyle(
+    color: red300,
+    // Define text styling properties as needed
+  );
+  static TextStyle red400TextStyle = TextStyle(
+    color: red400,
+    // Define text styling properties as needed
+  );
+  static TextStyle red500TextStyle = TextStyle(
+    color: red500,
+    // Define text styling properties as needed
+  );
+  static TextStyle red600TextStyle = TextStyle(
+    color: red600,
+    // Define text styling properties as needed
+  );
+  static TextStyle red700TextStyle = TextStyle(
+    color: red700,
+    // Define text styling properties as needed
+  );
+  static TextStyle red800TextStyle = TextStyle(
+    color: red800,
+    // Define text styling properties as needed
+  );
+  static TextStyle red900TextStyle = TextStyle(
+    color: red900,
+    // Define text styling properties as needed
+  );
+  static TextStyle blue100TextStyle = TextStyle(
+    color: blue100,
+    // Define text styling properties as needed
+  );
+  static TextStyle blue200TextStyle = TextStyle(
+    color: blue200,
+    // Define text styling properties as needed
+  );
+  static TextStyle blue300TextStyle = TextStyle(
+    color: blue300,
+    // Define text styling properties as needed
+  );
+  static TextStyle blue400TextStyle = TextStyle(
+    color: blue400,
+    // Define text styling properties as needed
+  );
+  static TextStyle blue500TextStyle = TextStyle(
+    color: blue500,
+    // Define text styling properties as needed
+  );
+  static TextStyle blue600TextStyle = TextStyle(
+    color: blue600,
+    // Define text styling properties as needed
+  );
+  static TextStyle blue700TextStyle = TextStyle(
+    color: blue700,
+    // Define text styling properties as needed
+  );
+  static TextStyle blue800TextStyle = TextStyle(
+    color: blue800,
+    // Define text styling properties as needed
+  );
+  static TextStyle blue900TextStyle = TextStyle(
+    color: blue900,
+    // Define text styling properties as needed
+  );
+}
